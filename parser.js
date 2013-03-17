dc14d83214df29d020346712d5e4454cbc172334
mati <matortiz@gmail.com>
2013-03-17 00:36:37 -0300

    creando algunos atributos del board dentro del obj principal

--- parser.js
+++ parser.js
@@ -1,7 +1,19 @@
 var trelloParser = {
 	items : [],
+	blocks: [],
+	board: {
+		id : '',
+		name : '',
+		description : ''
+	},
+
+	
 	loadFile : function() {
 		$.getJSON('ccs.json', function(data) {
+			trelloParser.board.id = data.id;
+			trelloParser.board.name = data.name;
+			trelloParser.board.description = data.desc;
+			console.log(trelloParser.board);
 			trelloParser.fillData(data);
 			trelloParser.printData();
 		});
@@ -10,7 +22,10 @@ var trelloParser = {
 	fillData : function(data) {
 		$.each(data, function(key, val) {
 			if(typeof(val) === 'object' && val !== null) {
-				trelloParser.items.push('<li><h3>' + key + '</h3></li>');
+				if(key !== 'actions') return true;
+				trelloParser.history(val);
+				//trelloParser.items.push('<li><h3>' + key + '</h3></li>');
+				trelloParser.items.push('<li><h3>' + key + ' <span class="details">-</span></h3></li>');
 				trelloParser.fillData(val != 'null' ? val : key);
 			} else {
 				//if(val === 'fabianemsantos')
@@ -19,10 +34,14 @@ var trelloParser = {
 		});
 	},
 
+	history : function(data) {
+
+	},
+
 	printData : function() {
 		$('<ul/>', {
 			'class': 'my-new-list',
 			html: trelloParser.items.join('')
-		}).appendTo('body');	
+		}).appendTo('.results');	
 	}
 };
